6be9f1a4034e87a1b369b415abf08b4175907d8b
John Pan <johnpan@Johns-MBP.gateway>
2018-08-26 22:07:37 +1000

    check validation

--- app/src/main/java/com/example/pan/assignment1/controller/OnClickAddTrackingListener.java
+++ app/src/main/java/com/example/pan/assignment1/controller/OnClickAddTrackingListener.java
@@ -2,11 +2,16 @@ package com.example.pan.assignment1.controller;
 
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.example.pan.assignment1.R;
+import com.example.pan.assignment1.helper.InputValidationCheck;
 import com.example.pan.assignment1.model.tracking.MeelEvent;
 import com.example.pan.assignment1.model.tracking.Tracking;
 import com.example.pan.assignment1.model.tracking.TrackingManager;
@@ -33,26 +38,25 @@ public class OnClickAddTrackingListener implements OnClickListener{
 
     @Override
     public void onClick(View v) {
-        try {
+        if (!InputValidationCheck.checkTrackingTitleIsEmpty(context,et) && !InputValidationCheck.checkDuplicateTrackingTitle(context,et)){
+            try {
+
+                Date startDate = TrackingManager.dateformat.parse(startTime.getSelectedItem().toString());
+                Date endDate = TrackingManager.dateformat.parse(endTime.getSelectedItem().toString());
+
+                Tracking t = new MeelEvent();
+                t.setTitle(et.getText().toString());
+                t.setTargetStartTime(startDate);
+                t.setTargetEndTime(endDate);
+                t.setTrackableId(Integer.parseInt(extra));
+                TrackingManager.addTracking(t);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
 
 
-            Date startDate = TrackingManager.dateformat.parse(startTime.getSelectedItem().toString());
-            Date endDate = TrackingManager.dateformat.parse(endTime.getSelectedItem().toString());
-
-            Tracking t = new MeelEvent();
-            t.setTitle(et.getText().toString());
-            t.setTargetStartTime(startDate);
-            t.setTargetEndTime(endDate);
-            t.setTrackableId(Integer.parseInt(extra));
-            TrackingManager.addTracking(t);
-        } catch (ParseException e) {
-            e.printStackTrace();
+            Intent intent = new Intent(context, TrackingList.class);
+            context.startActivity(intent);
         }
-
-
-
-
-        Intent intent = new Intent(context,TrackingList.class);
-        context.startActivity(intent);
     }
 }
